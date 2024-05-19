5046dffcef0efadae489de614fc74e43f8f98a05
VawnWuyest <nvanquyet810@gmail.com>
2024-05-19 21:17:17 +0700

    Create home and login page

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ const app = express();
 
 //connect to mongodb
 const mongoose = require('mongoose');
+const { default: Login } = require('./ui/Page/Login');
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://nguyenducduypc160903:oUJoajUVaiCRJC8U@eng-center-db.pvg0yqc.mongodb.net/?retryWrites=true&w=majority&appName=eng-center-db').then(() => {
   console.log('Connected to MongoDB');
 });
 
-app.get('/', (req, res) => {
-    //home page home.html
-    res.sendFile(__dirname + '/ui/home.html');
-
+app.get('/', (req, res) = {
+  res.send(__dirname + '/ui/Page/Home/home.html');
 });
 
 app.listen(3000, () => {
