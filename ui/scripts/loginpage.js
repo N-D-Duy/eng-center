d4eac836f5ebf3e8ba0092f2049177791d80624b
VawnWuyest <nvanquyet810@gmail.com>
2024-06-05 00:04:19 +0700

    add ui

--- ui/scripts/loginpage.js
+++ ui/scripts/loginpage.js
@@ -2,14 +2,14 @@ import { authenticate } from './loginManager.js';
 document.getElementById("btn-submit-login-c").addEventListener("click", login);
 
 export function login() {
-    alert('Tên người dùng hoặc mật khẩu không chính xác!');
     const username = document.getElementById('username').value;
     const password = document.getElementById('pword').value;
     console.log(username, password);
     //Check login but return true because test
     if (authenticate(username, password)) {
         localStorage.setItem('loggedIn', 'true');
-        window.location.href = 'index.html';     
+        //Phan quyen o day ví dụ role admin
+        window.location.href = 'admissions.html';     
     } else {
         alert('Tên người dùng hoặc mật khẩu không chính xác!');  
     }
